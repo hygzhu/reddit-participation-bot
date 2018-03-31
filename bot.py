4b1d6b1ae356ecd96daab6de51fdb96712374ac2
Haoyang <hygzhu@gmail.com>
2018-03-31 16:25:45 -0400

    Fixed the reply template

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ def bot_login():
                 user_agent = "User participation bot v0.1")
     return reddit
 
+
 def collectComments(reddit):
     """
     Collect data for as many comments as possible
@@ -86,6 +87,7 @@ def collectComments(reddit):
     print("Added {}, Ignored {}, Updated {}".format(added_count, ignored_count, updated_count))
     print("COMPLETED COLLECT COMMENTS AT TIME " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
 
+
 def collectSubmissions(reddit):
     """
     Collect data for as many submissions as possible
@@ -142,10 +144,21 @@ def getStats(reddit):
     """
     print("Getting stats from DB")
     user_comment_count = dict()
-    user_total_karma = dict()
+    user_total_comment_karma = dict()
     user_comment_ratio = dict()
 
+    user_submission_count = dict()
+    user_total_submission_karma = dict()
+    user_submission_ratio = dict()
+
     total_comments = 0
+    total_comment_karma = 0
+    total_submissions = 0
+    total_submission_karma = 0
+
+    all_comment_text = []
+    all_submission_text = []
+    all_submission_titles = []
 
     db = firebase.database()
     dt = datetime.datetime.fromtimestamp(time.mktime(time.gmtime()))
@@ -154,6 +167,28 @@ def getStats(reddit):
         new_dt = dt - datetime.timedelta(days=i)
         print("{}-{}-{}".format(new_dt.month, new_dt.day, new_dt.year))
         new_dt = "{}-{}-{}".format(new_dt.month, new_dt.day, new_dt.year)
+        
+        #fetch daily submissions
+        daily_submissions = db.child("submissions").child(new_dt).get().val()
+        if daily_submissions == None: 
+            print("No Submissions for {}".format(new_dt))
+            continue
+        daily_submissions = daily_submissions.values()
+        for submission in daily_submissions:
+            total_submissions += 1
+            total_submission_karma += int(submission['score'])
+            all_submission_text.append(str(submission['self_text']))
+            all_submission_titles.append(str(submission['title']))
+            if(str(submission['author']) in user_submission_count): 
+                user_submission_count[str(submission['author'])] += 1
+                user_total_submission_karma[str(submission['author'])] += int(submission['score'])
+            else:
+                user_submission_count[str(submission['author'])] = 1
+                user_total_submission_karma[str(submission['author'])] = int(submission['score'])
+
+        for key in user_submission_count:
+            user_submission_ratio[key] = user_total_submission_karma[key]/user_submission_count[key]
+        
         #fetch daily comments
         daily_comments = db.child("comments").child(new_dt).get().val()
         if daily_comments == None: 
@@ -162,26 +197,38 @@ def getStats(reddit):
         daily_comments = daily_comments.values()
         for comment in daily_comments:
             total_comments += 1
+            total_comment_karma += int(comment['score'])
+            all_comment_text.append(str(comment['body']))
             if(str(comment['author']) in user_comment_count): 
                 user_comment_count[str(comment['author'])] += 1
-                user_total_karma[str(comment['author'])] += int(comment['score'])
+                user_total_comment_karma[str(comment['author'])] += int(comment['score'])
             else:
                 user_comment_count[str(comment['author'])] = 1
-                user_total_karma[str(comment['author'])] = int(comment['score'])
+                user_total_comment_karma[str(comment['author'])] = int(comment['score'])
 
         for key in user_comment_count:
-            user_comment_ratio[key] = user_total_karma[key]/user_comment_count[key]
+            user_comment_ratio[key] = user_total_comment_karma[key]/user_comment_count[key]
+
+    words = dict()
+    for text in all_comment_text + all_submission_text + all_submission_titles:
+        for word in text.split(' '):
+            if word in words:
+                words[word] += 1
+            else: 
+                words[word] = 1
+    
 
     return strings.UWATERLOO_FRIDAY_POST.format(
     sub = "uwaterloo", 
     start = "{new_dt.month}-{new_dt.day}-{new_dt.year}".format(new_dt = dt - datetime.timedelta(days=8)), 
     end = "{new_dt.month}-{new_dt.day}-{new_dt.year}".format(new_dt = dt - datetime.timedelta(days=1)), 
-    total_submissions = 0, total_submission_karma = 0, total_unique_submitters = 0, total_comments = total_comments, total_comment_karma= 0, total_unique_commentors = 0,
-    s_most = [(0,0)], s_karma= [(0,0)], s_highest = [(0,0)], s_lowest = [(0,0)], top_submission_user = "", top_submission_karma = 0, top_submission_link = "",
-    c_most = collections.Counter(user_comment_count).most_common(5), c_karma = collections.Counter(user_total_karma).most_common(5),
-    c_highest = collections.Counter(user_comment_ratio).most_common(5), c_lowest = collections.Counter(user_comment_ratio).most_common()[-5:][::-1],
-    top_comment_user = "", top_comment_karma = 0, top_comment_link = "", 
-    common_words = "", goose_count = "", my_dude_count = "", facebook_count = "",
+    total_submissions = total_submissions, total_submission_karma = total_submission_karma, total_unique_submitters = len(user_submission_count), 
+    total_comments = total_comments, total_comment_karma= total_comment_karma, total_unique_commentors = len(user_comment_count),
+    s_most = collections.Counter(user_submission_count).most_common(5), s_karma = collections.Counter(user_total_submission_karma).most_common(5), 
+    s_highest =  collections.Counter(user_submission_ratio).most_common(5),
+    c_most = collections.Counter(user_comment_count).most_common(5), c_karma = collections.Counter(user_total_comment_karma).most_common(5),
+    c_highest = collections.Counter(user_comment_ratio).most_common(5),
+    goose_count = words['goose'], my_dude_count = words['dude'], facebook_count = words['facebook'],
     contact ="https://www.reddit.com/message/compose/?to=user-activity", source= "https://github.com/hygzhu/reddit-participation-bot", website="")
 
 
@@ -195,17 +242,18 @@ def replyToThread(reddit):
         if(submission.title == "Free Talk Friday"):
             print("Created at {}".format(submission.created))
             replyText = getStats(reddit)
-            print(replyText)
-            #submission.reply(replyText)
+            #print(replyText)
+            submission.reply(replyText)
             return
 
 
 def main():
     reddit = bot_login()
     #collectComments(reddit)
-    collectSubmissions(reddit)
-    #replyToThread(reddit)
+    #collectSubmissions(reddit)
+    replyToThread(reddit)
     #getStats(reddit)
+
 
 if __name__ == "__main__":
     main()
--- strings.py
+++ strings.py
@@ -5,7 +5,7 @@ UWATERLOO_FRIDAY_POST = """# **/r/{sub} Weekly Statistics Bot**
  | Overall Statistics
 :--|:--
 **Total Submissions**| {total_submissions} submissions
-**Total Submissions Karma**| {total_submission_karma} total karma
+**Total Submission Karma**| {total_submission_karma} total karma
 **Total Unique Submitters**| {total_unique_submitters} Users
 **Total Comments**| {total_comments} comments 
 **Total Comment Karma**| {total_comment_karma} total karma
@@ -17,9 +17,7 @@ UWATERLOO_FRIDAY_POST = """# **/r/{sub} Weekly Statistics Bot**
 :--|:--
 **Most Submissions**| /u/{s_most[0][0]}1 with {s_most[0][1]} Submissions
 **Most Submission Karma**| /u/{s_karma[0][0]}1 with {s_karma[0][1]} total Submission karma
-**Highest Submission karma**| /u/{s_highest[0][0]}1 with {s_highest[0][1]} average karma per Submission 
-**Lowest Submission Karma**| /u/{s_lowest[0][0]}1 with {s_lowest[0][1]} average karma per Submission 
-**Top Submission** |  /u/{top_submission_user}1 with {top_submission_karma}1 karma on {top_submission_link}
+**Highest Submission karma**| /u/{s_highest[0][0]}1 with {s_highest[0][1]} average karma per Submission
 
 &nbsp;
 
@@ -28,14 +26,11 @@ UWATERLOO_FRIDAY_POST = """# **/r/{sub} Weekly Statistics Bot**
 **Most Comments**| /u/{c_most[0][0]}1 with {c_most[0][1]} comments 
 **Most Comment Karma**| /u/{c_karma[0][0]}1 with {c_karma[0][1]} total comment karma
 **Highest Average karma**| /u/{c_highest[0][0]}1 with {c_highest[0][1]} average karma per comment
-**Lowest Average Karma**| /u/{c_lowest[0][0]}1 with {c_lowest[0][1]} average karma per comment 
-**Top Comment** |  /u/{top_comment_user}1 with {top_comment_karma}1 karma on {top_comment_link}
 
 &nbsp;
 
  | Misc Statistics
 :--|:--
-**Most Common Words**| {common_words}
 **Mr. Goose Thanked**| Mr. Goose thanked {goose_count} times 
 **Rocks on my dude** | My dude rocked {my_dude_count} times
 **Working at Facebook** | {facebook_count} people in 4A (menlo park btw) 
